e7300e1e3c46952623fcef8d0d57903ddad0e51d
iirvanard <irvan9110@gmail.com>
2024-07-16 04:10:46 +0700

    test 1

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager, login_user, logout_user, login_required, U
 from flask_wtf.csrf import CSRFProtect
 from celery import Celery
 
-db = SQLAlchemy()
+db = SQLAlchemy(session_options={"autoflush": False})
 login_manager = LoginManager()
 csrf = CSRFProtect()
 celery = Celery()
