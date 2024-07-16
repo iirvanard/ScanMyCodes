9e2b3987b4d262a5d1195c34b8d10158731d3f75
iirvanard <irvan9110@gmail.com>
2024-07-17 06:35:24 +0700

    env update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,8 @@
 
 if [ ! -f .env ]
 then
-  export $(cat .env | xargs)
+  export $(cat ./.env | xargs)
 fi
-
 
 if [ ! -d "migrations" ]; then
   flask db init
@@ -12,9 +11,10 @@ fi
 flask db migrate -m 'create initial tables'
 flask db upgrade
 
-
 if [ "$FLASK_ENV" = "production" ]; then
-  gunicorn run:app -b 0.0.0.0:5000
-else
+  gunicorn run:app -b 0.0.0.0:5000 
+else 
   flask run --debug
 fi
+
+
