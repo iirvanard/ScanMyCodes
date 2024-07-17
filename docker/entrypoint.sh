aceb0498424d1838d62120caf74698e1e1bccec1
iirvanard <irvan9110@gmail.com>
2024-07-17 13:41:30 +0700

    test add datax

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,7 +11,7 @@ flask db migrate -m 'create initial tables'
 flask db upgrade
 
 if [ "$FLASK_ENV" = "production" ]; then
-  gunicorn --bind=0.0.0.0 --timeout 600 run:app
+  gunicorn app:app --bind=0.0.0.0 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
 else 
   flask run --debug
 fi
