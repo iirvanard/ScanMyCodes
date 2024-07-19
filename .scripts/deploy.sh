afb83e5672bedec621f20f09b59cf56440e916a0
iirvanard <irvan9110@gmail.com>
2024-07-19 13:36:00 +0700

    test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,8 +4,10 @@ echo "Deployment started ..."
 
 git pull origin dev
 
+echo "restart tasks ..."
 sudo systemctl restart celery
 
+echo "restart scanmycodes ..."
 sudo systemctl restart scanmycodes
 
 echo "Deployment finished!"
