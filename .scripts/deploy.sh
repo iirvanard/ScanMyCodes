bc17e77266a112c44183990cb91be4a6ef620ef7
iirvanard <irvan9110@gmail.com>
2024-07-19 13:33:37 +0700

    test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,31 +4,8 @@ echo "Deployment started ..."
 
 git pull origin dev
 
-# for docker
-# docker compose build 
+sudo systemctl restart celery
 
-# docker compose up -d
-
-
-# native flask
-
-# restarting the celery 
-
-# Memeriksa apakah ada proses Celery worker yang berjalan
-if pgrep -f 'celery worker' > /dev/null; then
-    echo "Celery worker ditemukan. Menghentikan proses yang ada..."
-    sudo pkill -9 -f 'celery worker'
-else
-    echo "Tidak ada Celery worker yang berjalan."
-fi
-
-# Mengaktifkan virtual environment
-source venv/bin/activate
-
-# Menjalankan Celery worker
-echo "Menjalankan Celery worker..."
-celery -A app:celery worker --loglevel=info --detach
 sudo systemctl restart scanmycodes
-
 
 echo "Deployment finished!"
